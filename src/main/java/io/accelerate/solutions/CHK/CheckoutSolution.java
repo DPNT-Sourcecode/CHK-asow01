5592e01fb65895692d625c982304b60ba115f30c
sbx_user1051 <sbx_user1051@169.254.47.37>
2025-09-18 12:52:34 +0000

    Thu Sep 18 12:52:34 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,11 +1,48 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         //throw new SolutionNotImplementedException();
-    	return 100;
+    	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+    	for(char c: skus.toCharArray()) {
+    		map.put(c, map.getOrDefault(c, 0)+1);
+    	}
+    	int sum = 0;
+    	int quantity, div, mod;
+    	for(Map.Entry entry: map.entrySet()) {
+    		char item = (char) entry.getKey();
+    		switch (item) {
+    		case 'A':
+    			quantity = (int) entry.getValue();
+    			div = quantity/3;
+    			mod = quantity%3;
+    			sum += div*130 + mod*50;
+    			break;
+    		case 'B':
+    			quantity = (int) entry.getValue();
+    			div = quantity/2;
+    			mod = quantity%2;
+    			sum += div*45 + mod*30;
+    			break;
+    		case 'C':
+    			quantity = (int) entry.getValue();
+    			sum += quantity * 20;
+    			break;
+    		case 'D':
+    			quantity = (int) entry.getValue();
+    			sum += quantity * 15;
+    			break;
+    		default:
+    			throw new SolutionNotImplementedException(); 
+    		}
+    	}
     }
 }
+
 
